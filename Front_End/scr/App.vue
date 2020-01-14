a277cd3809951ed7e6bb1d48f34df597508d8527
oleg1601 <54068512+oleg1601@users.noreply.github.com>
2020-01-15 00:35:58 +0400

    Add files via upload

---
+++ Front_End/scr/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <Title v-bind:title="title" />
+        <Description/>
+        <Canvas/>
+    </div>
+</template>
+
+<script>
+
+import Title from './components/Title.vue'
+import Description from './components/Description.vue'
+import Canvas from './components/Canvas.vue'
+
+export default {
+    name: 'app',
+    components: {
+        Title,
+        Description,
+        Canvas
+    },
+    data: () => {
+        return {
+            title: "Shape Detector"
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
+
